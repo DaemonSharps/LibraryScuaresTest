5806606dc58fd8bb181ea110a473052c7222a5c0
Денис Смирнов <66mrpechenka66@gmail.com>
2020-10-05 01:01:15 +0300

    Add interfases and classes. Start work

---
+++ Interfaces/IArea.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryAreas.Interfaces
+{
+    public interface Area
+    {
+        double Area { get; set; }
+        double CalcArea();
+        void ShowArea();
+
+    }
+}
---
+++ Interfaces/IPolygons.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryAreas.Interfaces
+{
+    public interface Polygons
+    { int edgeCount { get; set; }
+    }
+}
